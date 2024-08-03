d39d30f044772ef66964c8ec98a383b0e28d77b8
Ilya Gulko <ilya@ilyagulko.com>
2024-08-03 15:56:02 -0400

    fix indentation in chat.js

--- template/js/chat.js
+++ template/js/chat.js
@@ -1,14 +1,14 @@
 document.addEventListener('click', function(e) {
-    if(e.target && e.target.classList.contains('expand-link')) {
-        e.preventDefault();
-        var messageId = e.target.getAttribute('data-message-id');
-        var fullMessage = document.getElementById('full-message-' + messageId);
-        if(fullMessage.style.display === 'none') {
-            fullMessage.style.display = 'block';
-            e.target.textContent = 'Show Less';
-        } else {
-            fullMessage.style.display = 'none';
-            e.target.textContent = 'Show More';
-        }
-    }
+	if(e.target && e.target.classList.contains('expand-link')) {
+		e.preventDefault();
+		var messageId = e.target.getAttribute('data-message-id');
+		var fullMessage = document.getElementById('full-message-' + messageId);
+		if(fullMessage.style.display === 'none') {
+			fullMessage.style.display = 'block';
+			e.target.textContent = 'Show Less';
+		} else {
+			fullMessage.style.display = 'none';
+			e.target.textContent = 'Show More';
+		}
+	}
 });
