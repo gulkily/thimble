c8b50933ff10d48331b7c4fed221ad8783c2386b
Ilya Gulko <ilya@ilyagulko.com>
2024-09-12 12:54:56 -0400

    fix indents in fix_indent.pl

--- template/perl/fix_indent.pl
+++ template/perl/fix_indent.pl
@@ -21,30 +21,30 @@ sub convert_spaces_to_tabs {
     # Detect the most common space indentation
     my @space_indents = $content =~ /^( +)/mg;
     if (@space_indents) {
-        my %count;
-        $count{$_}++ for @space_indents;
-        my $most_common_indent = (sort { $count{$b} <=> $count{$a} } keys %count)[0];
-        my $spaces_per_indent = length($most_common_indent);
+	my %count;
+	$count{$_}++ for @space_indents;
+	my $most_common_indent = (sort { $count{$b} <=> $count{$a} } keys %count)[0];
+	my $spaces_per_indent = length($most_common_indent);
 
-        # Replace space indentation with tabs
-        my @lines = split /\n/, $content;
-        my @converted_lines;
+	# Replace space indentation with tabs
+	my @lines = split /\n/, $content;
+	my @converted_lines;
 
-        for my $line (@lines) {
-            my $indent_count = 0;
-            while ($line =~ s/^( {$spaces_per_indent})//) {
-                $indent_count++;
-            }
-            push @converted_lines, ("\t" x $indent_count) . $line;
-        }
+	for my $line (@lines) {
+	    my $indent_count = 0;
+	    while ($line =~ s/^( {$spaces_per_indent})//) {
+		$indent_count++;
+	    }
+	    push @converted_lines, ("\t" x $indent_count) . $line;
+	}
 
-        $lines_changed = @lines - @converted_lines;
+	$lines_changed = @lines - @converted_lines;
 
-        # Join the lines and write back to the file
-        my $converted_content = join "\n", @converted_lines;
-        open $file, '>', $file_path or die "Cannot open file $file_path for writing: $!";
-        print $file $converted_content;
-        close $file;
+	# Join the lines and write back to the file
+	my $converted_content = join "\n", @converted_lines;
+	open $file, '>', $file_path or die "Cannot open file $file_path for writing: $!";
+	print $file $converted_content;
+	close $file;
     }
 
     return $lines_changed;
@@ -54,19 +54,19 @@ sub process_directory {
     my ($directory) = @_;
 
     find(
-        {
-            wanted => sub {
-                return unless -f;
-                return unless /\.(py|js|html|php|pl|rb|css)$/;
-                my $file_path = $File::Find::name;
-                my $lines_changed = convert_spaces_to_tabs($file_path);
-                if ($lines_changed > 0) {
-                    print "Converted $lines_changed lines in $file_path\n";
-                }
-            },
-            no_chdir => 1,
-        },
-        $directory
+	{
+	    wanted => sub {
+		return unless -f;
+		return unless /\.(py|js|html|php|pl|rb|css)$/;
+		my $file_path = $File::Find::name;
+		my $lines_changed = convert_spaces_to_tabs($file_path);
+		if ($lines_changed > 0) {
+		    print "Converted $lines_changed lines in $file_path\n";
+		}
+	    },
+	    no_chdir => 1,
+	},
+	$directory
     );
 }
 
